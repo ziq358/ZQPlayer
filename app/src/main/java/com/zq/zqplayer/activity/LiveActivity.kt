40d30dd523659b8f477210b2d194f83caa0887db
wuyanqiang <wuyanqiang>
2018-11-27 19:20:50 +0800

    更新

--- app/src/main/java/com/zq/zqplayer/activity/LiveActivity.kt
+++ app/src/main/java/com/zq/zqplayer/activity/LiveActivity.kt
@@ -18,6 +18,10 @@ import com.zq.zqplayer.fragment.live.RankingFragment
 import com.zq.zqplayer.fragment.live.TheHostFragment
 import com.zq.zqplayer.fragment.live.VIPListFragment
 import kotlinx.android.synthetic.main.item_chat.*
+import com.mob.wrappers.ShareSDKWrapper.share
+import cn.sharesdk.onekeyshare.OnekeyShare
+
+
 
 /**
  *author: wuyanqiang
@@ -117,7 +121,7 @@ class LiveActivity : BaseActivity<IBasePresenter>(){
             }
 
             override fun onShareClick() {
-                Toast.makeText(this@LiveActivity, "onShareClick", Toast.LENGTH_SHORT).show()
+                showShare()
             }
 
             override fun onBarrageClick() {
@@ -146,5 +150,32 @@ class LiveActivity : BaseActivity<IBasePresenter>(){
     }
 
 
+    private fun showShare() {
+        val oks = OnekeyShare()
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize()
+
+        // 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle("分享")
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://sharesdk.cn")
+        // text是分享文本，所有平台都需要这个字段
+        oks.text = "我是分享文本"
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+//        oks.setImagePath("/sdcard/test.jpg")//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn")
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本")
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name))
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn")
+
+        // 启动分享GUI
+        oks.show(this)
+    }
 
 }
