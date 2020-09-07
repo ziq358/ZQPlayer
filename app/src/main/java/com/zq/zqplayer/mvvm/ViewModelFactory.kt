d839da99bb377f9e5ed279bf0813912b32456cf6
wuyanqiang <wuyanqiang@inke.cn>
2020-09-07 17:52:13 +0800

    修改警告

--- app/src/main/java/com/zq/zqplayer/mvvm/ViewModelFactory.kt
+++ app/src/main/java/com/zq/zqplayer/mvvm/ViewModelFactory.kt
@@ -8,7 +8,7 @@ import com.zq.zqplayer.mvvm.recommend.RecommendViewModel
 import com.zq.zqplayer.mvvm.register.RegisterViewModel
 import javax.inject.Inject
 
-class ViewModelFactory : ViewModelProvider.NewInstanceFactory {
+class ViewModelFactory @Inject constructor() : ViewModelProvider.NewInstanceFactory() {
 
     @Inject lateinit var application: Application
 
@@ -16,20 +16,20 @@ class ViewModelFactory : ViewModelProvider.NewInstanceFactory {
     @Inject lateinit var registerViewModel: RegisterViewModel
     @Inject lateinit var recommendviewmodel: RecommendViewModel
 
-    @Inject
-    constructor() : super(){
-    }
-
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         //isAssignableFrom()方法是判断是否为某个类的父类
-        if(modelClass.isAssignableFrom(LoginViewModel::class.java)){
-            return loginViewModel as T
-        }else if(modelClass.isAssignableFrom(RegisterViewModel::class.java)){
-            return registerViewModel as T
-        }else if(modelClass.isAssignableFrom(RecommendViewModel::class.java)){
-            return recommendviewmodel as T
+        return when {
+            modelClass.isAssignableFrom(LoginViewModel::class.java) -> {
+                loginViewModel as T
+            }
+            modelClass.isAssignableFrom(RegisterViewModel::class.java) -> {
+                registerViewModel as T
+            }
+            modelClass.isAssignableFrom(RecommendViewModel::class.java) -> {
+                recommendviewmodel as T
+            }
+            else -> throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name) as Throwable
         }
-        throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name) as Throwable
     }
 
 }
