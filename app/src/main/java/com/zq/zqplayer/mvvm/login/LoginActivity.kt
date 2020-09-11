490e1eb750996f2797267f766cfc959e3c902962
wuyanqiang <wuyanqiang@inke.cn>
2020-09-11 15:47:59 +0800

    使用 协程 开工作线程

--- app/src/main/java/com/zq/zqplayer/mvvm/login/LoginActivity.kt
+++ app/src/main/java/com/zq/zqplayer/mvvm/login/LoginActivity.kt
@@ -23,6 +23,10 @@ import io.reactivex.Observable
 import io.reactivex.ObservableOnSubscribe
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
@@ -68,16 +72,15 @@ class LoginActivity : MvvmBaseActivity() {
 
         loginViewModel.userInfo.observe(this, object : Observer<UserInfoBean> {
             override fun onChanged(t: UserInfoBean?) {
-                var disposable = Observable.create(ObservableOnSubscribe<UserInfoBean> {
-                            AppDatabase.getDatabase(this@LoginActivity).userDao()?.insert(User(t?.userName!!, t.password!!))
-                            it.onNext(t!!)
-                        })
-                        .subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe {
-                            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
-                            finish()
-                        }
+                GlobalScope.launch(){
+                    Log.d("ziq", String.format("LoginActivity GlobalScope.launch %s  ", Thread.currentThread()) )
+                    launch(Dispatchers.IO) {
+                        Log.d("ziq", String.format("LoginActivity insert %s  ", Thread.currentThread()) )
+                        AppDatabase.getDatabase(this@LoginActivity).userDao()?.insert(User(t?.userName!!, t.password!!))
+                    }
+                    startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                    finish()
+                }
             }
         })
 
